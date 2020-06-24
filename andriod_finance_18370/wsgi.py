6f734d0defe21678a9d6aa2e50752dd17ee88822
Crowdbotics <team@crowdbotics.com>
2020-06-24 00:35:21 +0000

    Initial commit.

---
+++ andriod_finance_18370/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for andriod_finance_18370 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'andriod_finance_18370.settings')
+
+application = get_wsgi_application()
