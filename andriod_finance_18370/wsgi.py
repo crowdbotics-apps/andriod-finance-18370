0ee09b235e778305828860ac10e32cd781d8f53a
Crowdbotics <team@crowdbotics.com>
2020-06-24 00:36:31 +0000

    Update models

--- andriod_finance_18370/wsgi.py
+++ andriod_finance_18370/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'andriod_finance_18370.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "andriod_finance_18370.settings")
 
 application = get_wsgi_application()
